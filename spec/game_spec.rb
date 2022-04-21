37e32e93d4f8f0236dd9b868bceaf2ce05351bd1
zork <adampapirnik@gmail.com>
2022-04-21 14:49:08 -0500

    Implement #check? RSpec tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -15,7 +15,7 @@ describe Chess do
           [nil, nil, nil, nil, nil, nil, nil, nil],
           [nil, nil, nil, nil, nil, nil, nil, nil],
           [nil, nil, nil, nil, nil, nil, nil, nil],
-          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil, nil]
         ]
 
         stalemate_game = Chess.new(board, -1)
@@ -34,11 +34,70 @@ describe Chess do
           [nil, nil, nil, nil, nil, nil, nil, nil],
           [nil, nil, nil, nil, nil, nil, nil, nil],
           [nil, nil, nil, nil, nil, nil, nil, nil],
-          [nil, nil, nil, nil, nil, nil, Queen.new(1), nil],
+          [nil, nil, nil, nil, nil, nil, Queen.new(1), nil]
         ]
 
         non_stalemated_game = Chess.new(board, -1)
         result = non_stalemated_game.stalemate?
+        expect(result).to be false
+      end
+    end
+  end
+
+  describe '#check?' do
+    context 'When Black King is is in check' do
+      it 'returns true' do
+        board = [
+          [nil, nil, nil, nil, nil, nil, nil, King.new(-1)],
+          [nil, nil, nil, nil, nil, nil, Pawn.new(1), nil],
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, King.new(1), nil]
+        ]
+
+        black_check_game = Chess.new(board, -1)
+        result = black_check_game.check?
+        expect(result).to be true
+      end
+    end
+
+    context 'When White King is in check' do
+      it 'returns true' do
+        board = [
+          [nil, nil, nil, nil, nil, nil, nil, King.new(-1)],
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, Rook.new(-1), nil],
+          [nil, nil, nil, nil, nil, nil, King.new(1), nil]
+        ]
+
+        white_check_game = Chess.new(board, 1)
+        result = white_check_game.check?
+        expect(result).to be true
+      end
+    end
+
+    context 'When White King is not in check' do
+      it 'returns false' do
+        board = [
+          [nil, nil, nil, nil, nil, nil, nil, King.new(-1)],
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, Pawn.new(1), nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, nil, nil],
+          [nil, nil, nil, nil, nil, nil, King.new(1), nil]
+        ]
+
+        no_check_game = Chess.new(board, -1)
+        result = no_check_game.check?
         expect(result).to be false
       end
     end
